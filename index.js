ee5331a4831b2ec6e4dc35e94569534e4ed3196a
motcod <77477056+motcod@users.noreply.github.com>
2021-01-17 01:57:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,26 @@
 const axios = require("axios");
-const nodemailer = require("nodemailer");
-const smtpTransport = require("nodemailer-smtp-transport");
 
-const http = require('http');
-const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'I am looking for your Playstation :)\n'
-  res.end(msg);
-});
-
-const transporter = nodemailer.createTransport(
-  smtpTransport({
-    service: "Gmail",
-    auth: {
-      user: "maw6293@gmail.com",
-      pass: "D3ax8oQXCc",
-    }, 
-  })
-);
-// hashtag #preorder in subject for ifttt trigger
-const mailOptions = {
-  from: "maw6293@gmail.com",
-  to: "maw6293@gmail.com",
-  subject: "#preorderPS5",
-  text: "New stock",
-};
 
 const mailList = ["maw6293@gmail.com"];
 const urlGame = "https://www.bug.co.il/brand/ps5/marvel/spider/man/miles/morales/ultimate/edition"
 const urlPs5 = "https://www.bug.co.il/brand/ps5/ps5/console/digital";
 const addToCart = "addToCart(this";
+const theURL = "https://api.telegram.org/bot1515740642:AAG6xmaFJGJnVvLWqY9NHzhV3KF0KjiLp9c/sendMessage?chat_id=-491433102&text='NEW STOCK'"
 
 async function makeGetRequest() {
-  let res = await axios.get(urlPs5);
+  let res = await axios.get(urlGame);
   if (res.status >= 200 && res.status < 300) {
     console.log(`status is ${res.statusText}, status number is ${res.status}`);
     if (res.data.toLowerCase().includes(addToCart.toLowerCase())) {
       //pre order open
       console.log(`there is "add to cart" now`);
 
-      // send mail
-      mailList.forEach((address) => {
-        mailOptions.to = address;
-        transporter.sendMail(mailOptions, function (error, info) {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log("Email sent: " + info.response);
-          }
-        });
-      });
+      // MSG
+  
+      var xmlHttp = new XMLHttpRequest();
+	  xmlHttp.open( "GET", theURL, false ); // false for synchronous request
+      xmlHttp.send( null );
+	  
     } else {
       console.log(`still no "add to cart"`);
     }
