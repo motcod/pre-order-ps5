2d90bd33b5b3951f98c2c4e371dd99dda39a6b65
motcod <77477056+motcod@users.noreply.github.com>
2021-01-17 00:54:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const urlPs5 = "https://www.bug.co.il/brand/ps5/ps5/console/digital";
 const addToCart = "addToCart(this";
 
 async function makeGetRequest() {
-  let res = await axios.get(urlGame);
+  let res = await axios.get(urlPs5);
   if (res.status >= 200 && res.status < 300) {
     console.log(`status is ${res.statusText}, status number is ${res.status}`);
     if (res.data.toLowerCase().includes(addToCart.toLowerCase())) {
